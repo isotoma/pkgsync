62ea3b582a21e8f16e11a6ba06d3b27667305a6d
Alex Holmes <alex@alex-holmes.com>
2013-03-03 17:02:21 +0000

    Add unit tests for Metadata class

--- pkgsync/tests/test_meta.py
+++ pkgsync/tests/test_meta.py
@@ -1,5 +1,6 @@
-from unittest2 import TestCase
+import os
 import mock
+from unittest2 import TestCase
 
 from pkgsync.meta import Metadata, OldStyleMetadata
 from pkgsync.exceptions import InvalidDistribution
@@ -67,3 +68,59 @@ class OldStyleMetadataTest(TestCase):
         metadata = OldStyleMetadata(distribution)
         self.assertEqual(metadata.name, 'OldStylePackage')
         self.assertEqual(metadata.version, '1.2.3-beta4')
+
+
+class MetadataTest(TestCase):
+
+    def setUp(self):
+        self.assets_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets')
+
+    def asset_path(self, basename):
+        return os.path.join(self.assets_path, basename)
+
+    def test_invalid_dist(self):
+        distribution = mock.Mock(path=self.asset_path('AnOldStylePackage-1.5.4.tar.gz'))
+        with self.assertRaises(InvalidDistribution):
+            metadata = Metadata(distribution)
+
+    def test_upload(self):
+        distribution = mock.Mock(
+            path=self.asset_path('somefakepackage-0.0.0.tar.gz'),
+            md5_digest='0'*32,
+            basename='somefakepackage-0.0.0.tar.gz',
+            content='',
+        )
+        metadata = Metadata(distribution)
+        self.assertEqual(metadata.upload(), {
+            ':action': 'file_upload',
+            'protocol_version': '1',
+            'name': 'somefakepackage',
+            'version': '0.0.0',
+            'filetype': 'sdist',
+            'pyversion': '',
+            'md5_digest': '0'*32,
+            'content': ('somefakepackage-0.0.0.tar.gz', ''),
+        })
+
+    def test_register(self):
+        distribution = mock.Mock(path=self.asset_path('somefakepackage-0.0.0.tar.gz'))
+        metadata = Metadata(distribution)
+        self.assertEqual(metadata.register(), {
+            ':action': 'submit',
+            'author': u'Eddy Merckx',
+            'author_email': u'eddy.merckx@isotoma.com',
+            'classifiers': (),
+            'description': u'Long package description.',
+            'download_url': 'UNKNOWN',
+            'home_page': 'UNKNOWN',
+            'keywords': [],
+            'license': 'UNKNOWN',
+            'metadata_version': u'1.0',
+            'name': u'somefakepackage',
+            'obsoletes': (),
+            'platform': [u'UNKNOWN'],
+            'provides': (),
+            'requires': (),
+            'summary': u'Package description',
+            'version': u'0.0.0'
+        })
